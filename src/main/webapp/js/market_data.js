577979579759bbbbaba6cfa66d22eefefe2aa99e
zlm <qazse41@sina.cn>
2016-11-15 10:34:21 +0800

    图片

--- src/main/webapp/js/market_data.js
+++ src/main/webapp/js/market_data.js
@@ -1,7 +1,11 @@
 (function($){
     $('.showBtn').on('touchstart',function(){
         var hideTab = $(this).parent().parent().next();
-        hideTab.toggleSlide()
+        hideTab.css({
+            'opacity':1,
+            'top':'0.75rem'
+        });
+        hideTab.find('li').css('height','0.75rem')
     })
 
 
