292e2802562825f7906d198c020aead59ce55c6c
hyh <dfhjdfh@163.com>
2016-11-15 17:29:43 +0800

    11-15

--- src/main/webapp/js/welcome.js
+++ src/main/webapp/js/welcome.js
@@ -1,1 +1,24 @@
+$('#btn').on('click', function () {
+   var userName = $('#userName').val();
+   var password = $('#password').val();
+    console.log(username)
+    console.log(password)
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        data: {
+            'username': userName,
+            'password': password
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            location.href = data.requestPathA;
+        },
+        error: function (jqXHR) {
+            if (jqXHR.status == 400) {
 
+            }
+        }
+    })
+})
