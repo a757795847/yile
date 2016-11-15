578b4dd3f8d0a40a5ab1fa6c88a5531b5fcf0896
zlm <qazse41@sina.cn>
2016-11-15 09:46:56 +0800

    图片

--- src/main/webapp/js/market_data.js
+++ src/main/webapp/js/market_data.js
@@ -1,23 +1,8 @@
-(function(){
-    var showBtn = document.getElementsByClassName('showBtn');
-    function showTab(){
-        var hideNode ;
-        for(var i=0;i<showBtn.length;i++){
-            hideNode = showBtn[i].parentNode.parentNode.parentNode.childNodes;
-            console.log(hideNode);
-            for(var j=0;j<hideNode.length;j++){
-                if(hideNode[j].className == "hideTab"){
-                    console.log(hideNode[j].getAttribute('style').slice(':'));
-                    //hideNode[j].style.display = hideNode[j].getAttribute('style').slice(':')[1]=='none'?'block:'none';
-                }
-            }
-        }
-    }
+(function($){
+    $('.showBtn').on('touchstart',function(){
+        var hideTab = $(this).parent().parent().next();
+        hideTab.toggleSlide()
+    })
 
-    for(var i=0;i<showBtn.length;i++){
-        showBtn[i].onclick = function(){
-            showTab();
-        }
-    }
 
-})()
+})(jQuery)
