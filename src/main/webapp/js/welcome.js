ee0fd39a3f49cf1ede1be40a05aa1e692e804327
hyh <dfhjdfh@163.com>
2016-11-15 18:54:26 +0800

    11-15

--- src/main/webapp/js/welcome.js
+++ src/main/webapp/js/welcome.js
@@ -1,24 +1,32 @@
-$('#btn').on('click', function () {
-   var userName = $('#userName').val();
-   var password = $('#password').val();
-    console.log(username)
-    console.log(password)
-    $.ajax({
-        type: 'POST',
-        url: '/login',
-        data: {
-            'username': userName,
-            'password': password
-        },
-        dataType: 'json',
-        success: function (data) {
-            console.log(data);
-            location.href = data.requestPathA;
-        },
-        error: function (jqXHR) {
-            if (jqXHR.status == 400) {
+$(document).ready(function(){
+    $('.list input').iCheck({
+        checkboxClass: 'icheckbox_polaris',
+        radioClass: 'iradio_polaris',
+        increaseArea: '-10%'
+    });
+    $('#btn').on('click', function () {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        console.log(username)
+        console.log(password)
+        $.ajax({
+            type: 'POST',
+            url: '/login',
+            data: {
+                'username': username,
+                'password': password
+            },
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                location.href = data.requestPathA;
+            },
+            error: function (jqXHR) {
+                if (jqXHR.status == 400) {
 
+                }
             }
-        }
+        })
     })
-})
+
+});
