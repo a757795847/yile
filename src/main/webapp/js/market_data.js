693c6e752958ca4b77114021304496151888ef62
zlm <qazse41@sina.cn>
2016-11-15 19:13:58 +0800

    save

--- src/main/webapp/js/market_data.js
+++ src/main/webapp/js/market_data.js
@@ -1,6 +1,11 @@
 (function($){
+    $(document.body).infinite(50);
     $('.tabContent').on('touchstart',function(){
         function showTab(){
+            $('.showBtn').find('img').css({
+                'transform':'rotateX(0deg)',
+                'transform-origin':'50% 50%'
+            })
             $('.hideTab').css({
                 'opacity':0,
                 'transition':'opacity 0s'
@@ -14,6 +19,7 @@
 
         var hideTab = $(this).find('.hideTab');
         if(hideTab.css('opacity') == 0){
+            showTab();
             $(this).find('img').css({
                 'transform':'rotateX(180deg)',
                 'transform-origin':'50% 50%'
@@ -40,4 +46,14 @@
     })
 
 
+    var loading = false;  //状态标记
+    $(document.body).infinite().on("infinite", function() {
+        console.log('到底啦');
+        if(loading) return;
+        loading = true;
+        setTimeout(function() {
+            $("#list").append("<p> 我是新加载的内容 </p>");
+            loading = false;
+        }, 1500);   //模拟延迟
+    });
 })(jQuery)
