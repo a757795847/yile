945dd507ea42c5bb98b885310b755dd41f359742
hyh <dfhjdfh@163.com>
2016-11-16 16:19:27 +0800

    11-16 hyh

--- src/main/webapp/js/welcome.js
+++ src/main/webapp/js/welcome.js
@@ -25,13 +25,13 @@ $(document).ready(function(){
             dataType: 'json',
             success: function (data) {
                 if(data.msg_error=='验证码错误'){
-                    console.log('验证码错误');
+                    $.alert('验证码错误');
                 }else if(data.psd_beyond=='该用户连续输错4次密码，已被锁定！你可以到VM后台电脑端‘登录页面’【取回密码】来解锁和获取新的密码！'){
-                    alert(data.psd_beyond);
+                    $.alert('该用户连续输错4次密码，已被锁定！你可以到VM后台电脑端‘登录页面’【取回密码】来解锁和获取新的密码！');
                 }else if(data.user_disable=='该用户已被停用!'){
-                    alert(data.user_disable);
+                    $.alert('该用户已被停用!');
                 }else{
-                    alert(data.msg_nul);
+                    $.alert(' 请输入验证码');
 
                 }
 
