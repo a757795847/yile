25260b23d2e82b15f56842bc684c6c749be387da
zengriyong <510966612@qq.com>
2016-11-16 17:51:39 +0800

    save

--- src/main/java/com/jfinal/weixin/controller/AddDeviceController.java
+++ src/main/java/com/jfinal/weixin/controller/AddDeviceController.java
@@ -2,6 +2,7 @@ package com.jfinal.weixin.controller;
 
 import com.jfinal.aop.Before;
 import com.jfinal.interceptor.UserAuthInterceptor;
+import com.jfinal.kit.StrKit;
 import com.jfinal.weixin.sdk.api.ApiConfig;
 import com.jfinal.weixin.sdk.jfinal.ApiController;
 import com.jfinal.weixin.util.WeixinUtil;
@@ -19,12 +20,18 @@ public class AddDeviceController extends ApiController {
         String deviceId = getPara("deviceId");
         String deviceModel = getPara("deviceModel");
         String deviceData = getPara("deviceData");
-        //TODO 把这三个数据存入数据库中
+        if (StrKit.notBlank(deviceId) && StrKit.notBlank(deviceModel) && StrKit.notBlank(deviceData)) {
+            //TODO 把这三个数据存入数据库中
 
-        if (true) {
+            Boolean flag = true;
+            if (flag) {
+                renderJson("success", "添加成功");
+            } else {
+                renderJson("failed", "添加失败");
+            }
 
         } else {
-
+            renderJson("error", "请输入完整信息");
         }
     }
 
