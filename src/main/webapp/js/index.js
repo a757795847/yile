4c7d18a87b92b970a1a6f14bf347349eadac165c
hyh <dfhjdfh@163.com>
2016-11-24 14:13:30 +0800

    11-24 hyh

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function (){
    var session = $("#session").val();
     console.log(session);
 
-    if(session == 'false'){
+    //if(session == 'false'){
   $(".topLog").remove();
   $("#addtopLog").remove();
     $(".nav li").css("width","33%");
     $(".img1").css({"width":"26%","marginTop":"7%"});
     $(".img4").css({"width":"47%","marginTop":"13%"});
     $(".img2").css({"width":"41%","marginTop":"9%"});
-}
+//}
 });
 $('#Rightimg').on('touchstart',function(){
     console.log("d");
