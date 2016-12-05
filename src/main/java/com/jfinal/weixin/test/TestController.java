e64c9196843f4f925fcbeb4f095574b06bf14138
zengriyong <510966612@qq.com>
2016-12-05 18:40:34 +0800

    save

--- src/main/java/com/jfinal/weixin/test/TestController.java
+++ src/main/java/com/jfinal/weixin/test/TestController.java
@@ -3,6 +3,7 @@ package com.jfinal.weixin.test;
 import com.jfinal.aop.Before;
 import com.jfinal.interceptor.UserAuthInterceptor;
 import com.jfinal.weixin.sdk.api.ApiConfig;
+import com.jfinal.weixin.sdk.api.SnsAccessTokenApi;
 import com.jfinal.weixin.sdk.jfinal.ApiController;
 import com.jfinal.weixin.util.WeixinUtil;
 
@@ -15,10 +16,16 @@ public class TestController extends ApiController {
         return WeixinUtil.getApiConfig();
     }
 
-//    @Before(UserAuthInterceptor.class)
-    public void index(){
+    //    @Before(UserAuthInterceptor.class)
+    public void index() {
         String userId = getSessionAttr("userId");
         setAttr("test", userId);
         render("/index/index.jsp");
+    }
+
+    public static void main(String[] args) {
+        String appId = "wx6d37f5d05a8d301f";
+        String s = SnsAccessTokenApi.getAuthorizeURL(appId, "http://vmmis.levending.net/yile/oauth2", true);
+        System.out.println(s);
     }
 }
