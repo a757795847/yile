3b65524b6168872ce59eeb11df4874e61d0ddc82
zengriyong <510966612@qq.com>
2016-12-14 11:13:12 +0800

    save

--- src/main/webapp/js/welcome.js
+++ src/main/webapp/js/welcome.js
@@ -120,9 +120,9 @@ $("#image2").click(function(event){
                             $.alert('该用户连续输错4次密码，已被锁定！你可以到VM后台电脑端‘登录页面’【取回密码】来解锁和获取新的密码！');
                         } else if (data.user_disable == '该用户已被停用!') {
                             $.alert('该用户已被停用!');
-                        }else if(data.user_error =="用户名或密码错误！"){
+                        }else if(data.user_error == "用户名或密码错误"){
                             $.alert('用户名或密码错误');
-
+                            return ;
                         }else{
                             location.href = data.requestPathA;
                         }
