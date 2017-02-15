9c49c29d0bc3d65fb2bc078258ff73d92e194367
hyh <dfhjdfh@163.com>
2017-02-15 10:37:18 +0800

    hyh

--- src/main/webapp/js/market_data.js
+++ src/main/webapp/js/market_data.js
@@ -50,7 +50,7 @@
                     market += '<div class="tabContent"><div class="showTab"><ul><li >'+data[i].deviceid+'</li><li>'+data[i].saletime+'</li><li>'+data[i].outtype+'</li><li class="liCenter"><p class="ellipsisWord">'+data[i].mingcheng+'</p></li>';
                     market += '<li class="showBtn"><img src="../img/18.png" alt="下拉"></li></ul></div><div class="hideTab"><ul><li>轨道编号</li><li>进价</li>';
                     market += '<li>价格</li><li>支付方式</li><li></li></ul><ul><li>'+data[i].trackno+'</li><li>'+data[i].buyprice+'</li><li>'+data[i].price+'</li>';
-                    market += '<li class="liCenter"><p class="ellipsisWord">'+data[i].payway+'</p></li><li></li></ul></div></div>';
+                    market += '<li class="liCenter contextCenter"><p class="ellipsisWord">'+data[i].payway+'</p></li><li></li></ul></div></div>';
                 }
                 if(on){
                     $('#content').html(market);
@@ -59,7 +59,7 @@
                 }
                 if(data.length < 30){
                     $('.weui-infinite-scroll').css('display','none');
-                    if(on && data.length < 20){
+                    if(on && data.length < 25){
                         $('.noContent').css('display','none');
                     }else{
                         $('.noContent').css('display','block');
--- src/main/webapp/js/pay.js
+++ src/main/webapp/js/pay.js
@@ -35,7 +35,7 @@
                 for(var i=0;i < data.length ; i++ ){
                   //  stranidClass = data[i].flag == ''? 'retreat':'';
                     stranidState = data[i].flag == '退款处理'? 'paperOff':'';
-                    pay += '<div class="tabContent"><div class="showTab"><ul><li class="minName"><span>'+data[i].noncashName+'</span><span>'+data[i].deviceid+'</span></li><li>'+data[i].saletime.slice(5)+'</li><li>'+data[i].price+'</li><li class="liCenter"><p class="ellipsisWord">'+data[i].name+'</p></li>';
+                    pay += '<div class="tabContent"><div class="showTab"><ul><li class="minName"><span>'+data[i].noncashName+'</span><span>'+data[i].deviceid+'</span></li><li>'+data[i].saletime.slice(5)+'</li><li class="liCenter"><p class="ellipsisWord">'+data[i].price+'</p></li><li class="liCenter"><p class="ellipsisWord">'+data[i].name+'</p></li>';
                     pay += '<li class="showBtn"><img src="../img/18.png" alt="下拉"></li></ul></div><div class="hideTab"><ul><li>交易号(tranid)</li><li>支付者ID(openid)</li>';
                     pay += '<li>轨道</li><li>实际销售</li><li></li></ul><ul><li class="liCenter"><p class="ellipsisWord">'+data[i].tranid+'</p></li>';
                     pay += '<li class="liCenter"><p class="ellipsisWord">'+data[i].openid+'</p></li><li>'+data[i].trackno+'</li><li><i class='+stranidState+'></i>'+data[i].flag+'</li><li></li></ul></div></div>';
